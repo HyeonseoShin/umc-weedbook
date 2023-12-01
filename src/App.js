59a9fad52f1f9e22f9eed67d1a44aab85f7c919b
TonkaNut <hynnxi@gnu.ac.kr>
2023-12-02 07:47:32 +0900

    cmtDecFst

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App()
 {
-  const [id, setId] = useState("");
+  /*const [id, setId] = useState("");
   const [password, setpassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
@@ -49,36 +49,74 @@ function App()
     {
       setTimeout(() => setIsLoading(false), 1500);
     }
-  }
+  }*/
+
+  const [id, setId] = useState("");
+  const [pw, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case "id":
+        setId(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleClick = async (event) => {
+    if (id === "" || pw === "")
+    {
+      alert("Submit 이벤트가 중단되었습니다.");
+      event.preventDefault();
+    }
+
+    setIsLoading(true);
+
+    try
+    {
+      const response = await axios.post("http://localhost:8000/user/login",
+      {
+        id: id,
+        pw: pw,
+      }
+      );
+
+      localStorage.setItem("token", response.data.result.AccessToken);
+
+      console.log("post 요청 성공", response.data);
+    } catch (error) {
+      console.log("post 요청 실패", error);
+    } finally {
+      const newId = 25;
+      setId(newId);
+
+      localStorage.setItem("id", newId.toString());
+      setTimeout(() => setIsLoading(false), 1500);
+    }
+  };
 
   return (
-    <div className="App">
-      <div>
-        <div className="input">
-          <label htmlFor='id'>
-            ID
-          </label>
-          <input name='id' value={id} onChange={handleChange}>
+    <div>
+      <input name="id" value={id} onChange={handleChange}>
 
-          </input>
-        </div>
+      </input>
+      <input name="password" value={pw} type="password" onChange={handleChange}>
 
-        <div className='input2'>
-          <label htmlFor='password'>
-            Password
-          </label>
-          <input name='password' value={password} type='password' onChange={handleChange}>
+      </input>
+      <input type="submit" value={"로그인"} onClick={handleClick} disabled={isLoading}>
 
-          </input>
-        </div>
+      </input>
+      <LoadingModal show={isLoading}>
 
-          <input type='submit' value={"login"} onclick={handleClick} disabled={isLoading}>
-
-          </input>
-          <LoadingModal show = {isLoading}>
-
-          </LoadingModal>
-      </div>
+      </LoadingModal>
     </div>
   );
 }
@@ -91,9 +129,27 @@ function LoadingModal({ show })
   }
 
   return(
-    <div className='modal'>
-      <div className='content'>
-        Loading...
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+      }}
+    >
+      <div
+        style={{
+          padding: "20px",
+          backgroundColor: "white",
+          borderRadius: "10px",
+        }}
+      >
+        로딩 중...
       </div>
     </div>
   );
